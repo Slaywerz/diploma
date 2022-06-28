3e3d82585794ce378272922d1a2bbf454624f200
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2022-06-29 00:36:19 +0300

    added basic selenide elements

--- src/test/java/ru/netology/pages/PaymentInfo.java
+++ src/test/java/ru/netology/pages/PaymentInfo.java
@@ -1,4 +1,16 @@
 package ru.netology.pages;
 
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
 public class PaymentInfo {
+    private final SelenideElement cardNumber = Selenide.$("input[type='text'][placeholder='0000 0000 0000 0000']");
+    private final SelenideElement validMonth = Selenide.$("input[type='text'][placeholder='08']");
+    private final SelenideElement validYear = Selenide.$("input[type='text'][placeholder='22']");
+    private final SelenideElement cardHolderName = Selenide.$("fieldset div:nth-child(3) > span > span:nth-child(1) input");
+    private final SelenideElement cvcCode = Selenide.$("input[type='text'][placeholder='999']");
+    private final SelenideElement continueButton = Selenide.$(Selectors.byText("Продолжить"));
+    private final SelenideElement errorNotification = Selenide.$(".notification_status_error");
+    private final SelenideElement successNotification = Selenide.$(".notification_status_ok");
 }
