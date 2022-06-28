f544511553caf79bde4ffe4f41682b779516114d
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2022-06-29 01:10:36 +0300

    added methods with card and cardholder info

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,4 +1,37 @@
 package ru.netology.data;
 
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.util.Random;
+
 public class DataHelper {
+    private DataHelper() {
+    }
+
+
+    private static String getMonth() {
+        LocalDate localDate = LocalDate.now();
+        int month = localDate.getMonthValue();
+        return String.format("%02d", month);
+    }
+
+    private static String getYear() {
+        LocalDate localDate = LocalDate.now();
+        return String.format("%02d", localDate.getYear());
+    }
+
+    private static String getApprovedCard() {
+        return "4444 4444 4444 4441";
+    }
+
+    private static String getCVC() {
+        Random random = new Random();
+        return String.format("%03d", random.nextInt(100 - 1));
+    }
+
+    private static String getName() {
+    Faker faker = new Faker();
+    return faker.name().firstName() + " " + faker.name().lastName();
+    }
 }
