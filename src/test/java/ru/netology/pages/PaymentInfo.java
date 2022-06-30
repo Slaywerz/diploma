902e8aa00da98c24b93ae9a5c63b0a648744755e
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2022-06-30 23:29:09 +0300

    Added SelenideElement for validCard in PaymentInfo.java

--- src/test/java/ru/netology/pages/PaymentInfo.java
+++ src/test/java/ru/netology/pages/PaymentInfo.java
@@ -1,13 +1,15 @@
 package ru.netology.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
 
 public class PaymentInfo {
     private final SelenideElement cardNumber = Selenide.$("input[type='text'][placeholder='0000 0000 0000 0000']");
-    private final SelenideElement validMonth = Selenide.$("input[type='text'][placeholder='08']");
-    private final SelenideElement validYear = Selenide.$("input[type='text'][placeholder='22']");
+    private final SelenideElement monthValue = Selenide.$("input[type='text'][placeholder='08']");
+    private final SelenideElement yearValue = Selenide.$("input[type='text'][placeholder='22']");
     private final SelenideElement cardHolderName = Selenide.$("fieldset div:nth-child(3) > span > span:nth-child(1) input");
     private final SelenideElement cvcCode = Selenide.$("input[type='text'][placeholder='999']");
     private final SelenideElement continueButton = Selenide.$(Selectors.byText("Продолжить"));
@@ -17,4 +19,14 @@ public class PaymentInfo {
     private final SelenideElement invalidYearNotification = Selenide.$(Selectors.byText("Неверно указан срок действия карты"));
     private final SelenideElement invalidFormatNotification = Selenide.$(Selectors.byText("Неверный формат"));
     private final SelenideElement necessarilyFieldNotification = Selenide.$(Selectors.byText("Поле обязательно для заполнения"));
+
+    public SelenideElement validCard (DataHelper.CardInfo cardInfo) {
+        cardNumber.setValue(cardInfo.getCardNumber());
+        monthValue.setValue(cardInfo.getMonth());
+        yearValue.setValue(cardInfo.getYear());
+        cardHolderName.setValue(cardInfo.getCardHolder());
+        cvcCode.setValue(cardInfo.getCvcCode());
+        continueButton.click();
+        return successNotification.shouldBe(Condition.visible);
+    }
 }
