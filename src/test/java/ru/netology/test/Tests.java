0ffe1cb8ddb6db0006d859b28180d16e9ef682fb
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2022-07-05 23:38:00 +0300

    added positive tests

--- src/test/java/ru/netology/test/Tests.java
+++ src/test/java/ru/netology/test/Tests.java
@@ -3,6 +3,11 @@ package ru.netology.test;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.pages.PaymentChoice;
+import ru.netology.pages.PaymentInfo;
 
 
 public class Tests {
@@ -15,5 +20,45 @@ public class Tests {
     @AfterAll
     static void showDown() {
         Selenide.closeWindow();
+    }
+
+    @Test
+    @DisplayName("Show success notification for approved debit card")
+    void shouldBeShowSuccessNotification() {
+        var paymentChoice = new PaymentChoice();
+        paymentChoice.debitPayment();
+        var paymentInfo = new PaymentInfo();
+        paymentInfo.fields(DataHelper.validValues());
+        paymentInfo.setSuccessNotification();
+    }
+
+    @Test
+    @DisplayName("Show error notification for declined debit card")
+    void shouldBeShowErrorNotification() {
+        var paymentChoice = new PaymentChoice();
+        paymentChoice.debitPayment();
+        var paymentInfo = new PaymentInfo();
+        paymentInfo.fields(DataHelper.declinedValues());
+        paymentInfo.setErrorNotification();
+    }
+
+    @Test
+    @DisplayName("Show success notification for approved Credit card")
+    void shouldShowSuccessNotificationForCreditCard() {
+        var paymentChoice = new PaymentChoice();
+        paymentChoice.creditPayment();
+        var paymentInfo = new PaymentInfo();
+        paymentInfo.fields(DataHelper.validValues());
+        paymentInfo.setSuccessNotification();
+    }
+
+    @Test
+    @DisplayName("Show error notification for declined Credit card")
+    void shouldShowErrorNotificationForCreditCard() {
+        var paymentChoice = new PaymentChoice();
+        paymentChoice.creditPayment();
+        var paymentInfo = new PaymentInfo();
+        paymentInfo.fields(DataHelper.declinedValues());
+        paymentInfo.setErrorNotification();
     }
 }
