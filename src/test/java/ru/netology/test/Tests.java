e510fbe7726b90e6789ba56e344271064c6833f0
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2022-07-05 23:37:19 +0300

    added setup and showdown in Tests.java

--- src/test/java/ru/netology/test/Tests.java
+++ src/test/java/ru/netology/test/Tests.java
@@ -1,4 +1,19 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+
+
 public class Tests {
+
+    @BeforeEach
+    void setUp() {
+        Selenide.open("http://localhost:8080/");
+    }
+
+    @AfterAll
+    static void showDown() {
+        Selenide.closeWindow();
+    }
 }
