3199d8b022697318b6b6717b7f2cebca3d1c9879
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2022-07-17 16:03:41 +0300

    added fields for DB

--- src/test/java/ru/netology/pages/PaymentInfo.java
+++ src/test/java/ru/netology/pages/PaymentInfo.java
@@ -10,16 +10,27 @@ import java.time.Duration;
 
 public class PaymentInfo {
     private final SelenideElement cardNumber = Selenide.$("input[type='text'][placeholder='0000 0000 0000 0000']");
+
     private final SelenideElement monthValue = Selenide.$("input[type='text'][placeholder='08']");
+
     private final SelenideElement yearValue = Selenide.$("input[type='text'][placeholder='22']");
+
     private final SelenideElement cardHolderName = Selenide.$("fieldset div:nth-child(3) > span > span:nth-child(1) input");
+
     private final SelenideElement cvcCode = Selenide.$("input[type='text'][placeholder='999']");
+
     private final SelenideElement continueButton = Selenide.$(Selectors.byText("Продолжить"));
+
     private final SelenideElement errorNotification = Selenide.$(".notification_status_error");
+
     private final SelenideElement successNotification = Selenide.$(".notification_status_ok");
+
     private final SelenideElement invalidMonthNotification = Selenide.$(Selectors.byText("Неверно указан срок действия карты"));
-    private final SelenideElement invalidYearNotification = Selenide.$(Selectors.byText("Неверно указан срок действия карты"));
+
+    private final SelenideElement invalidYearNotification = Selenide.$(Selectors.byText("Истёк срок действия карты"));
+
     private final SelenideElement invalidFormatNotification = Selenide.$(Selectors.byText("Неверный формат"));
+
     private final SelenideElement necessarilyFieldNotification = Selenide.$(Selectors.byText("Поле обязательно для заполнения"));
 
     public void fields(DataHelper.CardInfo cardInfo) {
@@ -31,12 +42,26 @@ public class PaymentInfo {
         continueButton.click();
     }
 
+    public void fieldsForDB(DataHelper.CardInfo cardInfo) {
+        cardNumber.setValue(cardInfo.getCardNumber());
+        monthValue.setValue(cardInfo.getMonth());
+        yearValue.setValue(cardInfo.getYear());
+        cardHolderName.setValue(cardInfo.getCardHolder());
+        cvcCode.setValue(cardInfo.getCvcCode());
+        continueButton.click();
+        try {
+            Thread.sleep(8000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void setSuccessNotification() {
-        successNotification.shouldBe(Condition.visible, Duration.ofSeconds(30));
+        successNotification.shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 
     public void setErrorNotification() {
-        errorNotification.shouldBe(Condition.visible, Duration.ofSeconds(30));
+        errorNotification.shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 
     public void setInvalidFormatNotification() {
